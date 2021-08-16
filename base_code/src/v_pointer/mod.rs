e1604c8909c8e71c1b90b789cf9ef4ef9427007f
AH_Pirate <15212726737@163.com>
2021-08-16 19:42:24 +0800

    add: add deref，结引用

--- base_code/src/v_pointer/mod.rs
+++ base_code/src/v_pointer/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 #[cfg(test)]
 mod pointer_test {
     use crate::v_pointer::List::{Cons, Nil};
@@ -18,4 +20,49 @@ mod pointer_test {
 enum List {
     Cons(i32, Box<List>),
     Nil,
+}
+
+#[cfg(test)]
+mod deref_trait {
+    use crate::v_pointer::MyBox;
+
+    #[test]
+    fn one() {
+        let x = 5;
+        let y = &x;
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+    #[test]
+    fn two() {
+        let x = 5;
+        let y = Box::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+    #[test]
+    fn third() {
+        let x = 5;
+        let y = MyBox::new(x);
+
+        assert_eq!(5, x);
+        assert_eq!(5, *y);
+    }
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
